6f001b08551d5781e7576fd05fa2e4a56271953e
ipcplindia <ipcplindia@gmail.com>
2026-05-06 23:39:42 +0530

    Create page.tsx

---
+++ File 1: app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/sections/Hero';
+import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}

987ba71008dc9f042af439ea132d076c87ae3069
ipcplindia <ipcplindia@gmail.com>
2026-05-06 23:36:13 +0530

    Create site.config.ts

---
+++ File 6: config/site.config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "BHOLENATH PRODUCTIONS",
+  tagline: "SHIVAYONIC MUSIC",
+  presentation: "BHOLENATH PRODUCTIONS AND SHIVAYONIC MUSIC PRESENTS",
+  founder: "BETA OF BHOLENATH JI... Amit Poddar...",
+  description: "Where Divinity Inspires Creativity. Original cinematic worlds. Sacred sound. Future-ready storytelling.",
+  youtube: "https://www.youtube.com/channel/UCUIbQKh-7ZHjqRF9dJwzdlw",
+  youtubeMusic: "https://music.youtube.com/@bholenathproductionsofficial?si=iFhS_jIppsqW52Dm",
+  instagram: "https://www.instagram.com/shivayonicmusic",
+  ogImage: "/images/og.jpg"
+};
